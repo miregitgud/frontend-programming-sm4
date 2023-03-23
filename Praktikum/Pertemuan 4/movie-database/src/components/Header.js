c69726f16f2fbe2c0b719c93d8da747fde0e1608
/ <39770337+Mirezka@users.noreply.github.com>
2023-03-23 22:18:20 +0700

    minor changes

--- Praktikum/Pertemuan 4/movie-database/src/components/Header.js
+++ Praktikum/Pertemuan 4/movie-database/src/components/Header.js
@@ -3,7 +3,13 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-            <h1>Bagian header dari proyek react ini.</h1>
+        <nav style={{flexDirection: 'row', fontWeight: 'bold', fontSize: '3vh' }}>
+        <a href="/">Home</a> | 
+        <a href="/about"> About</a> |
+        <a href="/contact"> Contact</a> 
+        </nav>
+        <hr></hr>
+            <h1>Selamat datang di website saya!</h1>
         </header>
     );
 }
